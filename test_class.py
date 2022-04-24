2d117b1ec3db13ed409e6b228691378da28c9502
Chris Borg <cborg09@gmail.com>
2022-04-24 15:52:48 -0700

    added doc string

--- test_class.py
+++ test_class.py
@@ -1,4 +1,17 @@
 class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+    '''sample dostring
+    Note:
+        Do not include the `self` parameter in the ``Args`` section.
+
+    Args:
+        msg (str): Human readable string describing the exception.
+        code (:obj:`int`, optional): Error code.
+
+    Attributes:
+        msg (str): Human readable string describing the exception.
+        code (int): Exception error code.
+    '''
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
